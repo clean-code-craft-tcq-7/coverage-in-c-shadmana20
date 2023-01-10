aed6527e2f03ff8da4dcfaf63b68436449aaf7eb
shadmana20 <115541042+shadmana20@users.noreply.github.com>
2023-01-10 19:43:32 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -25,5 +25,22 @@ TEST_CASE("F2_T3") {
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -34) == TOO_LOW);
 }
 
-
+TEST_CASE("F2_T4") {
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 37) == TOO_HIGH);
+}
+TEST_CASE("F2_T5") {
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 34) == NORMAL);
+}
+TEST_CASE("F2_T6") {
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, -34) == TOO_LOW);
+}
+TEST_CASE("F2_T7") {
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 37) == TOO_HIGH);
+}
+TEST_CASE("F2_T8") {
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 34) == NORMAL);
+}
+TEST_CASE("F2_T9") {
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, -34) == TOO_LOW);
+}
 
