c1cd79fe71e6bbb19bf34e49306435c6c270e11a
shadmana20 <115541042+shadmana20@users.noreply.github.com>
2023-01-10 20:02:02 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -4,8 +4,8 @@
 #include "typewise-alert.h"
 
 char buf[50];
-const char* srcHigh = "To:  a.b@c.com","Hi, the temperature is too high\n"
-const char* srcLow  = "To:  a.b@c.com","Hi, the temperature is too low\n"
+const char* srcHigh = "To:  a.b@c.com","Hi, the temperature is too high\n";
+const char* srcLow  = "To:  a.b@c.com","Hi, the temperature is too low\n";
 BatteryCharacter batteryCharTest1,batteryCharTest2;
 
 
