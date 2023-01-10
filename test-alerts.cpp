4c2fb3009deda24ba66b16592887129c3639de51
shadmana20 <115541042+shadmana20@users.noreply.github.com>
2023-01-10 20:16:38 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -57,7 +57,7 @@ TEST_CASE("F2_T6") {
 
 TEST_CASE("send email alert high") {
 batteryCharTest1.coolingType = PASSIVE_COOLING;
-checkAndAlert(TO_EMAIL,batteryCharTest1,50,&buf[0]);
+checkAndAlert(TO_EMAIL,batteryCharTest1,50);
 REQUIRE(strcmp( buf,srcHigh)==0);
 }
 
@@ -65,7 +65,7 @@ REQUIRE(strcmp( buf,srcHigh)==0);
 
 TEST_CASE("send email alert low") {
 batteryCharTest2.coolingType = HI_ACTIVE_COOLING;
-checkAndAlert(TO_EMAIL,batteryCharTest2,-10,&buf[0]);
+checkAndAlert(TO_EMAIL,batteryCharTest2,-10);
 REQUIRE(strcmp( buf,srcLow)==0);
 }
 
