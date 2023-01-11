f6e308e7535718ba5a6281470ce7ab2da4cdefc9
shadmana20 <115541042+shadmana20@users.noreply.github.com>
2023-01-11 18:14:34 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -7,9 +7,7 @@
 
 
 
-const char* recipent = "To : a.b@c.com";
-const char* srcHigh = "Hi, the temperature is too high\n";
-const char* srcLow  = "Hi, the temperature is too low\n";
+
 BatteryCharacter batteryCharTest1,batteryCharTest2;
 
 
