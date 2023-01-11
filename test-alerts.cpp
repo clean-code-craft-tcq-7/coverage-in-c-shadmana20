a40bf52921127e867a9016b2995fa067f756fb0f
shadmana20 <115541042+shadmana20@users.noreply.github.com>
2023-01-11 19:23:44 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -10,6 +10,8 @@ const char* srcHigh = "Hi, the temperature is too high\n";
 const char* srcLow  = "Hi, the temperature is too low\n";
 BatteryCharacter batteryCharTest1,batteryCharTest2;
 
+TempFlags Tempflag;
+
 
 
 TEST_CASE("LOW limit value test") {
