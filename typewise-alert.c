fae18747316140b7b921d7586dac20adfc113dbd
shadmana20 <115541042+shadmana20@users.noreply.github.com>
2023-01-11 20:02:32 +0530

    Update typewise-alert.c

--- typewise-alert.c
+++ typewise-alert.c
@@ -1,7 +1,7 @@
 #include "typewise-alert.h"
 #include <stdio.h>
 
-TempFlags Tempflag = {.FlagHighTemp = TEMPLOW ,.FlagLowTemp = TEMPHIGH,.FlagTypeMail = UNUSED, .FlagTypeController = UNUSED};
+
 int CoolingUpperLimits[MAX_COOLING_TYPE] = {35 , 40, 45};
 
 void (*AlertTarget_func_ptr[TO_MAX])(BreachType) = {&sendToController, &sendToEmail};
@@ -35,31 +35,5 @@ void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double
   
 }
 
-void sendToController(BreachType breachType) 
-{
- 
-  const unsigned short header = 0xfeed;
-  printf("%x : %x\n", header, breachType);
-  Tempflag.FlagTypeController = USED;
-}
 
-void sendToEmail(BreachType breachType) 
-{
-  
-  
-  Tempflag.FlagTypeMail = USED;
-  const char* recepient = "a.b@c.com";
-  if(breachType == TOO_LOW)
-  {
-    printf("To: %s\n", recepient);
-    printf("Hi, the temperature is too low\n");
-    Tempflag.FlagLowTemp = TEMPLOW;
-  }
-
-  if(breachType == TOO_HIGH)
-  {
-     printf("To: %s\n", recepient);
-     printf("Hi, the temperature is too high\n");
-    Tempflag.FlagHighTemp = TEMPHIGH;
-  }
 }
