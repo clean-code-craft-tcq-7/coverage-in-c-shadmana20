56098473c110783cb7fbf5635b6052835d7aa3c6
shadmana20 <115541042+shadmana20@users.noreply.github.com>
2023-01-11 19:19:36 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -9,7 +9,7 @@ const char* recipent = "To : a.b@c.com";
 const char* srcHigh = "Hi, the temperature is too high\n";
 const char* srcLow  = "Hi, the temperature is too low\n";
 BatteryCharacter batteryCharTest1,batteryCharTest2;
-TempFlags Tempflag = {.FlagHighTemp = TEMPLOW ,.FlagLowTemp = TEMPHIGH,.FlagTypeMail = UNUSED, .FlagTypeController = UNUSED};
+
 
 
 TEST_CASE("LOW limit value test") {
